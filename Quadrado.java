93b36bb7f1d4e2e69a181bda2add8eefc05f62f1
Alícia Constantino Costa <lykamusic@hotmail.com>
2018-08-27 17:09:49 -0300

    Add files via upload

---
+++ Quadrado.java
@@ -0,0 +1,12 @@
+
+public class Quadrado extends Retangulo {
+
+	public Quadrado(int a) { super(a,a);}
+	public Quadrado(int x, int y, int a) { super(x,y,a,a); }
+
+	
+	public String toString() {
+		return("Quadrado("+x0+":"+y0+":"+altura+")");
+		
+	}
+}
---
+++ Teste.java
@@ -0,0 +1,17 @@
+
+public class Teste {
+
+	static void Teste(Figura f,String s) {
+		System.out.println(s+" ==> " + f.toString()+
+				          "area:"+f.area() +
+				          "perimetro:"+f.perimetro());
+	}
+
+	public static void main(String[] args) {
+		Retangulo r1 = new Retangulo(1, 2 ,5 , 10); 
+		Retangulo r2 = new Retangulo(5, 10);
+        teste(r1, "r1");
+        teste(r2, "r2");
+	}
+
+}
